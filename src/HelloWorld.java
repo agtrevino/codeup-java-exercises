3a79bb6f971fb6bac53e8c98c78bad783a45f387
Aaron Trevino <Aaron@aarons-mbp.attlocal.net>
2018-07-23 21:10:28 -0500

    java exercise file added

---
+++ src/HelloWorld.java
@@ -0,0 +1,100 @@
+//        Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+//
+//        Change your code to assign the value 123 to myNumber.
+//
+//        Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does?
+//        Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
+//
+//        Copy and paste the following code blocks one at a time and execute them
+//
+//
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        What is the difference between the above code blocks? Explain why the code outputs what it does.
+//
+//        Try to create a variable named class. What happens?
+//
+//        Rewrite the following expressions using the relevant shorthand assignment operators:
+//
+//
+//        int x = 4;
+//        x = x + 5;
+//
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+//        byte rating = 5;
+//        short aztecTheatre = 3435;
+//        int ATTCenter = 3455435;
+//        long Alamodome =21434356545555L;
+//        float price = 23.44f;
+//        double grade = 88.95;
+//
+//        //reference types
+//        Long anotherVenue = 123123123123123L;
+//        String name = "Aaron";
+//
+//        System.out.println("Price = " + price);
+//        System.out.println("Grade = " + grade);
+//
+//        char initialFN = 'f';
+//        char gender = 'c';
+//        boolean doYouHaveQuestions = true;
+//
+//        System.out.println("Name = " + name);
+//
+//        String cohort = "Voyageurs";
+//
+//        final int PAGES = 364;
+//
+//        rating++;
+//        System.out.println("Rating = " + rating);
+//
+//        System.out.println("Pages = " + PAGES + 1);
+//
+//        //explicit
+//
+//        double pi = 3.1416;
+//        int almostPi = (int) pi;
+//
+//        double populationThatLikesVanilla = 123.23;
+//        byte chartVanilla = (byte) populationThatLikesVanilla;
+//
+//        System.out.println(pi);
+//        System.out.println(almostPi);
+
+        //exercise
+
+        int favoriteNumber = 66;
+        System.out.println(favoriteNumber);
+
+        String myString = "Aaron T.";
+        System.out.println(myString);
+
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+    }
+
+}

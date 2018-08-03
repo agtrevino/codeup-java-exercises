c7904fb5230ec3fe3e1ecc116b02856a6d48f48b
Aaron Trevino <Aaron@Aarons-MacBook-Pro.local>
2018-08-03 14:28:28 -0500

    added student

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,13 +1,39 @@
 package grades;
+import java.util.ArrayList;
 
 public class Student {
 
-    private class StudentName{
+    private String name;
+    private ArrayList<Integer> grades;
 
+    Student(String name){
+        this.name = name;
+        grades = new ArrayList<>();
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(String name){
+        return name;
     }
 
-    private class StudentGrades{
-
+    public void addGrade(int grade){
+        grades.add(grade);
     }
 
+    public double getGradeAverage(){
+        int total = 0;
+        for(int grade : grades){
+            total+=grade;
+        }
+        return total / grades.size();
+    }
+
+    public static void main(String[] args) {
+        Student kappa = new Student("Kappa");
+        kappa.grades.add(56);
+        kappa.grades.add(65);
+        System.out.println(kappa.grades);
+        System.out.println(kappa.getGradeAverage());
+    }
 }
